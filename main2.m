e3183d25ad22f43e5ae83b398b40302dc4f369e2
mshojaee3 <110466741+mshojaee3@users.noreply.github.com>
2026-02-19 19:23:33 +0100

    Refactor main2.m with updated settings

--- main2.m
+++ main2.m
@@ -1,20 +1,22 @@
+
 clc;
 clear
 
 %% === Settings ===
-user   = '????????';  % Use your own username. If you don't have one, ask Michael to create it.
+user   = '??????';  % Use your own username. If you don't have one, ask Michael to create it.
 server = 'orion.fnb.maschinenbau.tu-darmstadt.de';
 
 agent  = '-agent ';   % Use this if you use Pageant/SSH keys. If not, set: agent = ''.
 
 localFolder = pwd;    % Local folder to upload (current MATLAB folder)
-remoteBase  = '/home/cps/shojaee';   % Change this to your own remote path (e.g., /home/cps/<yourUser>)
-entryFile   = 'main.py';            % Script to run on Orion
+remoteBase  = '/home/cps/??????';   % Change this to your own remote path (e.g., /home/cps/<yourUser>)
+entryFile   = '??????.py';            % Script to run on Orion
 
 % Download mode:
 % true  -> download everything from the run folder
 % false -> download only *.csv and *.png
 downloadAll = false;
+
 
 %% === Minimal run ===
 runName     = ['run_' datestr(now,'yyyymmdd_HHMMss')];
@@ -31,3 +33,4 @@ else
     system(sprintf('pscp -batch %s%s@%s:"%s/*.csv" "%s"', agent,user,server,remoteRunDir,destLocal));
     system(sprintf('pscp -batch %s%s@%s:"%s/*.png" "%s"', agent,user,server,remoteRunDir,destLocal));
 end
+
